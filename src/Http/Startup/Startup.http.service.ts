6401a21389e76d87ba9698af4a9d88fe02284f76
Oleh Saveniuk <oleh.saveniuk@gmail.com>
2022-10-07 15:59:29 +0200

    Display startup card list

--- src/Http/Startup/Startup.http.service.ts
+++ src/Http/Startup/Startup.http.service.ts
@@ -3,6 +3,11 @@ import { Startup, StartupDTO } from "../../Types/Startup";
 import StartupMapper from "./Startup.mapper";
 
 export class StartupHttpService {
+  public static async getStartups(): Promise<any> {
+    const response = await axios.get<StartupDTO[]>(`/api/startups`, { params: { all: true } });
+    return response.data.map(el => StartupMapper.map(el));
+  }
+
   public static async getStartupById(id: string | number): Promise<Startup> {
     const response = await axios.get<StartupDTO>(`/api/startups/${id}`);
     return StartupMapper.map(response.data);
